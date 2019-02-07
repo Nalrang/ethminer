09e7d0d75a9557fe96554f7e21d230430088aac3
Nalrang <jang00625@gmail.com>
2019-02-07 18:21:52 +0900

    Update main.cpp

--- ethminer/main.cpp
+++ ethminer/main.cpp
@@ -254,6 +254,8 @@ public:
 
         app.add_option("--farm-retries", m_PoolSettings.connectionMaxRetries, "", true)->check(CLI::Range(0, 99999));
 
+        app.add_option("--retry-delay", m_PoolSettings.delayBeforeRetry, "", true)
+        
         app.add_option("--work-timeout", m_PoolSettings.noWorkTimeout, "", true)
             ->check(CLI::Range(180, 99999));
 
